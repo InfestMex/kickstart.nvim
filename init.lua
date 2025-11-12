5eea791b740419cfff1e30c075b42b88ccf5f407
vaquilar <vaquilar@gk-software.com>
2025-11-11 20:19:43 -0600

    Add json to disable onsave format

--- init.lua
+++ init.lua
@@ -905,7 +905,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true, xml = true, xsd = true, dtx = true }
+        local disable_filetypes = { c = true, cpp = true, json = true, xml = true, xsd = true, dtx = true }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
