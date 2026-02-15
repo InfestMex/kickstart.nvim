a1f10d5ac9b52a937f9cc5d57d0cfb7376783c9a
viaguila <victor.aguilar@oracle.com>
2026-02-15 12:46:54 -0600

    apply transparetn background to all screens in neovime

--- init.lua
+++ init.lua
@@ -278,7 +278,16 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-  'xiyaowong/transparent.nvim',
+  {
+    'xiyaowong/transparent.nvim',
+    opts = {
+      extra_groups = { 'NormalFloat', 'FloatBorder', 'SignColumn', 'EndOfBuffer' },
+    },
+    config = function(_, opts)
+      require('transparent').setup(opts)
+      vim.cmd 'TransparentEnable'
+    end,
+  },
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
