00ca84f3843113da7283986feb51cf8974b02045
vaquilar <vaquilar@gk-software.com>
2026-03-05 18:48:56 -0600

    Fix cmx pos bat, remove validation check

--- custom/files/gk/CMX/fix_bash_defaultVars.sh
+++ custom/files/gk/CMX/fix_bash_defaultVars.sh
@@ -10,4 +10,7 @@ CMD_FILE="C:/DEV_HOME/CMX/ws-pos/git/pos/build/pos-full/pos-full-sandbox/_sandbo
 # Comment out lines 43 through 47
 sed -i '39,46{/^REM /!s/^/REM /}' "$CMD_FILE"
 
+# Remove consistency checker
+sed -i 's/-Dpos.plugin_mode=%PLUGIN_MODE% com.gk_software.pos.Main/-Dpos.plugin_mode=%PLUGIN_MODE% -Dgkr.opos.consistency-checker.enabled=false com.gk_software.pos.Main/g' C:/DEV_HOME/CMX/ws-pos/git/pos/build/pos-full/pos-full-sandbox/_sandbox__0037/run_tpos.cmd
+
 echo "Lines 39 to 46 in $CMD_FILE have been commented out."
