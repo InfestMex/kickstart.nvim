c38560e502a5ae44cfabe9ef118301389eb40905
vaquilar <vaquilar@gk-software.com>
2026-03-08 12:55:11 -0600

    fix CMX pos run

--- custom/files/gk/CMX/POS_sandbox__0037.sh
+++ custom/files/gk/CMX/POS_sandbox__0037.sh
@@ -3,6 +3,15 @@
 # ==================================
 echo "Running run_tpos script"
 
+# Get the absolute path of the directory where the script is located.
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Compile custom code
+source $SCRIPT_DIR/../set-env-variables.sh
+export JAVA_HOME="$SHARED_HOME/TOOLS/java/1.8.0_111"
+mvn clean install -Dfast -f C:/DEV_HOME/CMX/ws-pos/git/pos/client/mod-functions-client-cst/pom.xml
+mvn clean install -Dfast -f C:/DEV_HOME/CMX/ws-pos/git/pos/build/pos-full/pos-full-deployment/pom.xml
+
 # Change to the directory where this script is located
 cd "$(dirname "$0")"
 
@@ -14,7 +23,6 @@ cd /c/DEV_HOME/CMX/ws-pos/git/pos/build/pos-full/pos-full-sandbox/_sandbox__0037
 
 # Run the POS
 echo "Running run_tpos.cmd..."
-export JAVA_HOME="$SHARED_HOME/TOOLS/java/1.8.0_111"
 ./run_tpos.cmd
 
 RETURN_CODE=$?
